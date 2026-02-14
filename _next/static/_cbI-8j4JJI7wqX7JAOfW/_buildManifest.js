f6b9ae28a8406b07a559b6f512aefb11d21894fe
rizd027 <alfarizd027@gmail.com>
2026-02-14 01:30:20 +0000

    Deploying to gh-pages from @ rizd027/Greenetix-Boy@d60d8530eb07cd563fb7f52a2c4dd222b458fa10 🚀

--- _next/static/_cbI-8j4JJI7wqX7JAOfW/_buildManifest.js
+++ _next/static/_cbI-8j4JJI7wqX7JAOfW/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Greenetix-Boy//_next/:path+",
+        "destination": "/Greenetix-Boy/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
