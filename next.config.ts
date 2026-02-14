d60d8530eb07cd563fb7f52a2c4dd222b458fa10
rizd027 <rizd027@example.com>
2026-02-14 08:29:40 +0700

    fix: add assetPrefix and trailingSlash for correctly loaded assets on GH Pages

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,13 @@
 import type { NextConfig } from "next";
 
 const isProd = process.env.NODE_ENV === 'production';
+const repo = 'Greenetix-Boy';
 
 const nextConfig: NextConfig = {
     output: 'export',
-    basePath: isProd ? '/Greenetix-Boy' : '',
+    basePath: isProd ? `/${repo}` : '',
+    assetPrefix: isProd ? `/${repo}/` : '',
+    trailingSlash: true,
     images: {
         unoptimized: true,
     },
